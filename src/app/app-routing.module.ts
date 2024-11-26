88656eece5cf919bd0c7684fafe832e710661b85
Enmanuel Ochoa <eochoa0007@gmail.com>
2024-11-26 15:31:31 -0500

    Renderized data for single component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListProductComponent } from './components/list-product/list-product.component';
+import { ListClientComponent } from './components/list-client/list-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ListProductComponent},
+  {path: 'clients', component: ListClientComponent},
+  //{path: 'crear-empleado', component: CrearEmpleadoComponent},
+  // {path: '', redirectTo: 'empleados', pathMatch: 'full'},
+  // { path: '**', redirectTo: '/empleados' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
